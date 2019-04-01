0316a14db8b307c275bd781d222a4308abe33c5d
zhoulinmu <48110262+zhoulinmu@users.noreply.github.com>
2019-04-01 08:56:32 +0800

    Update Functions.java

--- WordCount/src/Functions.java
+++ WordCount/src/Functions.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 interface  Functions {
    public abstract void exit();
-   public abstract void  wordofnumber(ArrayList<Map.Entry<String, Integer>> list);
-   public abstract void beforeK(ArrayList<Map.Entry<String, Integer>> list);
+   public abstract void  wordOfNumber(ArrayList<Map.Entry<String, Integer>> list);
+   public abstract void beForeK(ArrayList<Map.Entry<String, Integer>> list);
    public abstract void write(ArrayList<Map.Entry<String, Integer>> list,String filename);
 }
